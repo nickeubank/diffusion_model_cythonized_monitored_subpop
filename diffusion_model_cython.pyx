3d8da4ae9ad6980cb32683974098d3e1acde8c23
Nick Eubank <nickeubank@gmail.com>
2017-03-16 20:55:29 -0700

    split counting steps to threshold into one function and recording infection rate after given steps into another

--- diffusion_model_cython.pyx
+++ diffusion_model_cython.pyx
@@ -133,41 +133,41 @@ class SIModel(object):
 
 
 
-def diffusion_run(graph, p, number_of_runs, 
-                  thresholds=None,
-                  steps_at_which_to_evaluate=None,
+
+def diffusion_run_threshold(graph, p, number_of_runs, 
                   num_starting_infections=None,  
                   initially_infected_nodes=None,
-                  max_iter=None,
+                  max_iter=1000,
+                  thresholds=[0.1, 0.25, 0.5, 0.75],
                   agg_function=np.mean,
                   include_finish_counts=False,
                   tests=False):
 
     """ 
-    Run diffusion model and report back either (a) number of steps required to 
-    infect shares of nodes set by threshold, or (b) steps of model at which to 
-    report share infected. Note that thresholds can run into problems if 
-    thresholds are never reached -- those values become NaN, potentially causing
-    problems when aggregating. 
+    Run diffusion model and report back number of steps required to 
+    infect shares of nodes set by threshold. 
+
+    Note that if a threshold is never reached, a NaN will be returned
+    for that run. Those NaN will eventually be fed to the agg_function.
+    Keep that in mind when picking an agg_function!
+
+    Use the include_finish_counts argument to get back count of 
+    number of times thresholds were reached. 
 
     graph:  iGraph object on which to run model
     p:      probability infection spreads across an edge in each step. 
-    number_of_runs: Number of times to run model. 
-    thresholds: infection thresholds to be evaluated.
-            Must be monotonic from smallest to largest. 
-            Cannot be combined with steps_at_which_to_evaluate
-    steps_at_which_to_evaluate: model steps at which to record
-            share covered. 
-            Must be monotonic from smallest to largest. 
-            Cannot be combined with thresholds. 
     num_starting_infections: Number of nodes initially infected. 
             will be selected uniformly at random. Cannot be combined
             with initially_infected_nodes. 
     initially_infected_nodes: list of specific nodes to be set as 
             infected at start of simulation. Cannot be combined 
             with num_starting_infections. 
+    number_of_runs: Number of times to run model. 
     max_iter: maximum number of steps allowed in simulation. 
             Default 1000. 
+    thresholds: infection thresholds to be evaluated.
+            Must be monotonic from smallest to largest. 
+            Default [0.1, 0.25, 0.5, 0.75].
     agg_function: function used to aggregate all the runs. 
             Default is np.mean. 
     include_finish_counts: Return DataFrame with column of 
@@ -177,114 +177,121 @@ def diffusion_run(graph, p, number_of_runs,
     tests: run internal integrity tests. Adds to running time, 
             mostly for internal purposes. 
     
-    """    
+    """
 
-    # Check if measuring at thresholds, or reporting at steps:
-    if thresholds is None and steps_at_which_to_evaluate is None:
-        raise ValueError("Must specify thresholds or steps_at_which_to_evaluate")
+    # Tests!
+    if tests:
+        test_suite()
+
+    # make sure monotonic
+    diffs = np.diff(np.array(thresholds))
+
+    if not (diffs > 0).all():
+        raise ValueError("Thresholds must be strictly monotonic")
+
+    if np.max(thresholds) > 1:
+        raise ValueError("Thresholds must be between 0 and 1")        
+
+    results = pd.DataFrame(np.nan, index=thresholds, columns=range(number_of_runs))
+
+    # Run 
+    for run in range(number_of_runs):
+
+        # Setup model.                   
+        si_model = SIModel(graph=graph, p=p, num_starting_infections=num_starting_infections, 
+                           initially_infected_nodes=initially_infected_nodes, tests=tests)
     
-    if thresholds is not None and steps_at_which_to_evaluate is not None:
-        raise ValueError("Cannot combine thresholds and steps_at_which_to_evaluate")
+        for i in range(0, max_iter):
+        
 
-    if include_finish_counts and thresholds is None:
-        raise ValueError("include_finish_counts can only be combined with thresholds")        
+            # Check coverage
+            for threshold in thresholds:
+                if pd.isnull(results.loc[threshold, run]) and si_model.share_infected >= threshold:                
+                    results.loc[threshold, run] = i
+            
+            # If last threshold hit, break out
+            if pd.notnull(results.loc[thresholds[-1], run]): 
+                break
+            
+            # Iterate!
+            si_model.step(i)
 
-    if max_iter and thresholds is None:
-        raise ValueError("max_iter can only be combined with thresholds")        
+
+    result = results.apply(agg_function, axis=1) 
+    finishes = results.apply(lambda x: x.count() / number_of_runs, axis=1)
+
+    if include_finish_counts:
+        result = pd.concat({"scores":result, 'finishes':finishes}, axis=1)
+
+    return result
+
+
+def diffusion_run_steps(graph, p, number_of_runs, 
+                  steps_at_which_to_evaluate,
+                  num_starting_infections=None,  
+                  initially_infected_nodes=None,
+                  agg_function=np.mean,
+                  tests=False):
+
+    """ 
+    Run diffusion model and report back share infected after set number
+    of steps. 
+
+    graph:  iGraph object on which to run model
+    p:      probability infection spreads across an edge in each step. 
+    number_of_runs: Number of times to run model. 
+    steps_at_which_to_evaluate: model steps at which to record
+            share covered. 
+            Must be monotonic from smallest to largest. 
+    num_starting_infections: Number of nodes initially infected. 
+            will be selected uniformly at random. Cannot be combined
+            with initially_infected_nodes. 
+    initially_infected_nodes: list of specific nodes to be set as 
+            infected at start of simulation. Cannot be combined 
+            with num_starting_infections. 
+    agg_function: function used to aggregate all the runs. 
+            Default is np.mean. 
+    tests: run internal integrity tests. Adds to running time, 
+            mostly for internal purposes. 
+    
+    """    
 
 
     # Tests!
     if tests:
         test_suite()
 
+    steps = np.array(steps_at_which_to_evaluate)
+    diffs = np.diff(steps)
 
-    #######
-    # Run with thresholds
-    #######
-    if thresholds is not None and steps_at_which_to_evaluate is None:
+    if not (diffs > 0).all():
+        raise ValueError("Steps must be monotonic")
 
-        diffs = np.diff(np.array(thresholds))
+    if np.array(steps).dtype != 'int64':
+        raise ValueError("steps must be integers")        
 
-        if not (diffs > 0).all():
-            raise ValueError("Thresholds must be strictly monotonic")
+    results = pd.DataFrame(np.nan, index=steps_at_which_to_evaluate, columns=range(number_of_runs))
 
-        if np.max(thresholds) > 1:
-            raise ValueError("Thresholds must be between 0 and 1")        
+    for run in range(number_of_runs):
 
-        if max_iter is None:
-            max_iter = 1000
+        # Setup model.                   
+        si_model = SIModel(graph=graph, p=p, num_starting_infections=num_starting_infections, 
+                           initially_infected_nodes=initially_infected_nodes, tests=tests)
+            
 
-        results = pd.DataFrame(np.nan, index=thresholds, columns=range(number_of_runs))
-
-        # Run 
-        for run in range(number_of_runs):
-
-            # Setup model.                   
-            si_model = SIModel(graph=graph, p=p, num_starting_infections=num_starting_infections, 
-                               initially_infected_nodes=initially_infected_nodes, tests=tests)
+        for i in range(0, max(steps_at_which_to_evaluate)+1):
         
-            for i in range(0, max_iter):
-            
+            # Check coverage
+            if i in steps_at_which_to_evaluate:
+                results.loc[i, run] = si_model.share_infected
 
-                # Check coverage
-                for threshold in thresholds:
-                    if pd.isnull(results.loc[threshold, run]) and si_model.share_infected >= threshold:                
-                        results.loc[threshold, run] = i
-                
-                # If last threshold hit, break out
-                if pd.notnull(results.loc[thresholds[-1], run]): 
-                    break
-                
-                # Iterate!
-                si_model.step(i)
+            # Iterate!
+            si_model.step(i)
 
 
-        result = results.apply(agg_function, axis=1) 
-        finishes = results.apply(lambda x: x.count() / number_of_runs, axis=1)
+    result = results.apply(agg_function, axis=1) 
 
-        if include_finish_counts:
-
-            result = pd.concat({"scores":result, 'finishes':finishes}, axis=1)
-
-        return result
-
-    #######
-    # Run Counting Steps
-    #######
-
-    if thresholds is None and steps_at_which_to_evaluate is not None:
-
-        steps = np.array(steps_at_which_to_evaluate)
-        diffs = np.diff(steps)
-
-        if not (diffs > 0).all():
-            raise ValueError("Steps must be monotonic")
-
-        if np.array(steps).dtype != 'int64':
-            raise ValueError("steps must be integers")        
-
-        results = pd.DataFrame(np.nan, index=steps_at_which_to_evaluate, columns=range(number_of_runs))
-
-        for run in range(number_of_runs):
-
-            # Setup model.                   
-            si_model = SIModel(graph=graph, p=p, num_starting_infections=num_starting_infections, 
-                               initially_infected_nodes=initially_infected_nodes, tests=tests)
-                
-
-            for i in range(0, max(steps_at_which_to_evaluate)+1):
-            
-                # Check coverage
-                if i in steps_at_which_to_evaluate:
-                    results.loc[i, run] = si_model.share_infected
-
-                # Iterate!
-                si_model.step(i)
-
-
-        result = results.apply(agg_function, axis=1) 
-
-        return result
+    return result
     
 
 #####
@@ -298,12 +305,12 @@ def test_suite():
     line.add_edges([(0,1)])    
     
     # Start with 1 of 2, should spread from half (starting seed) to full from 0 to 1. 
-    test_result = diffusion_run(graph=line, p=1, number_of_runs=2, num_starting_infections=1,
+    test_result = diffusion_run_threshold(graph=line, p=1, number_of_runs=2, num_starting_infections=1,
                                 thresholds=[0.1, 0.25, 0.5, 0.75])
     assert_series_equal(test_result, pd.Series([0.0,0,0,1],index = [0.1, 0.25, 0.5, 0.75]))
 
     # If start with 2, trigger right away
-    test_result = diffusion_run(graph=line, p=1, number_of_runs=2, num_starting_infections=2,
+    test_result = diffusion_run_threshold(graph=line, p=1, number_of_runs=2, num_starting_infections=2,
                                 thresholds=[0.1, 0.25, 0.5, 0.75])
     assert_series_equal(test_result , pd.Series([0.0,0,0,0],index = [0.1, 0.25, 0.5, 0.75]))                    
 
@@ -312,13 +319,13 @@ def test_suite():
     line = ig.Graph()
     line.add_vertices(range(5))
     line.add_edges([(0,1), (1,2), (2,3), (3,4)])    
-    test_result = diffusion_run(graph=line, p=1, number_of_runs=10, initially_infected_nodes=[0],
+    test_result = diffusion_run_steps(graph=line, p=1, number_of_runs=10, initially_infected_nodes=[0],
                                 steps_at_which_to_evaluate=range(5))
     assert_series_equal(test_result , pd.Series([0.2, 0.4, 0.6, 0.8, 1]))                    
 
 
     line.add_vertices([5, 6, 7])
-    test_result = diffusion_run(graph=line, p=1, number_of_runs=10, initially_infected_nodes=[0],
+    test_result = diffusion_run_steps(graph=line, p=1, number_of_runs=10, initially_infected_nodes=[0],
                                 steps_at_which_to_evaluate=range(10))
     assert_series_equal(test_result , pd.Series([0.125, 0.25, 0.375, 0.5, 0.625, 0.625, 0.625, 0.625, 0.625, 0.625]))                    
 
@@ -328,7 +335,7 @@ def test_suite():
     sparse = ig.Graph()
     sparse.add_vertices(4)
     sparse.add_edges([(0,1), (2,3)])
-    test_result = diffusion_run(graph=sparse, number_of_runs=1, p=1, num_starting_infections=1,
+    test_result = diffusion_run_threshold(graph=sparse, number_of_runs=1, p=1, num_starting_infections=1,
                                 thresholds=[0.1, 0.25, 0.5, 0.75])
     assert_series_equal(test_result , pd.Series([0.0,0,1,np.nan],index = [0.1, 0.25, 0.5, 0.75]))                      
     
@@ -343,7 +350,7 @@ def test_suite():
     run = 1000
     output = pd.Series(np.nan*run)
     for i in range(run):
-        r = diffusion_run(graph=ig.Graph.Full(2), p=0.2, number_of_runs=1, thresholds=[1], initially_infected_nodes={0})
+        r = diffusion_run_threshold(graph=ig.Graph.Full(2), p=0.2, number_of_runs=1, thresholds=[1], initially_infected_nodes={0})
         output.loc[i] = r.loc[1]
 
     try:       
@@ -361,7 +368,7 @@ def test_suite():
     run = 100
     output = pd.Series(np.nan*run)
     for i in range(run):
-        r = diffusion_run(graph=line, p=0.7, thresholds=[1], number_of_runs=1, initially_infected_nodes={0})
+        r = diffusion_run_threshold(graph=line, p=0.7, thresholds=[1], number_of_runs=1, initially_infected_nodes={0})
         output.loc[i] = r.loc[1]
 
     output.mean() - 9
